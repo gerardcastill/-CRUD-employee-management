1081ed58e209b6c5b5dca6566550271d75e99e86
Gerardo Castillo <41881493+gerardcastill@users.noreply.github.com>
2023-11-06 11:03:27 -0600

    Delete src directory

--- src/services/withRouter.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import { useNavigate} from 'react-router-dom';
-
-export const withRouter = (Component) => (props) => {
-  const navigate = useNavigate();
-  return <Component {...props} navigate={navigate} />;
-};
